c2e00e608be6504bebe8f8492e2ee345e52e1b62
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-08 20:28:13 +0300

    update

--- CleanupUtill.py
+++ CleanupUtill.py
@@ -9,53 +9,28 @@ from __future__ import annotations
 
 import json
 import os
-from dataclasses import dataclass
+from itertools import chain
 from pathlib import Path
 from typing import Callable
 from typing import Dict
+from typing import Iterable
+from typing import Optional
 from typing import Sequence
+from typing import TextIO
 
 _WORK_DIR = Path(__file__).parent.absolute()
-
-
-def __gen_table() -> Dict[str, str]:
-    table = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i',
-             'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
-             'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch',
-             'ъ': '`', 'ы': 'y', 'ь': '`', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
-    table.update({cyrillic.upper(): latin.upper() for cyrillic, latin in table.items()})
-    return table
-
-
-_TABLE = __gen_table()
-
-
-def to_camel_snake_case(s: str) -> str:
-    """Camel_Snake_Case"""
-    return '_'.join(map(lambda word: word.capitalize(), s.split()))
-
-
-def to_camel_case(s: str) -> str:
-    """CamelCase"""
-    return ''.join(map(lambda word: word.capitalize(), s.replace("_", " ").split()))
-
-
-def transliterate(s: str) -> str:
-    """Bykvi"""
-    return "".join(_TABLE.get(char, char) for char in s)
-
-
-def transliterate_to_camel_snake_case(s: str):
-    """Bykvi_Mnogo"""
-    return transliterate(to_camel_snake_case(s))
 
 
 def _get_workdir_size_bytes() -> int:
     return sum(p.stat().st_size for p in _WORK_DIR.rglob('*'))
 
 
-def _set_shell_color(color_code: int) -> None:
-    os.system(f"color {color_code:02X}")
+def _set_shell_color(color_code: int) -> int:
+    return os.system(f"color {color_code:02X}")
+
+
+def _shell_timeout(duration: int) -> int:
+    return os.system(f"timeout {duration}")
 
 
 def _get_size_str(size: int) -> str:
@@ -70,26 +45,40 @@ def _get_size_str(size: int) -> str:
     return f"{size} {unit}"
 
 
-@dataclass
+def _make_glob(path: Path, extensions: Sequence[str]):
+    return chain(*(path.rglob(f"*.{extension}") for extension in extensions))
+
+
+FileNameTransformer = Callable[[Path], str]
+
+
 class FileMover:
     """Настройка перемещение файлов"""
-    path: Path
-    target: Sequence[str]
-    transformer: Callable[[str], str] = to_camel_snake_case
 
-    def __post_init__(self) -> None:
-        self.path = Path(self.path).absolute()
+    def __init__(self, path: str, target: Sequence[str], transformer: Optional[str] = None) -> None:
+        """
+        Агент каталога
+        path: Относительный путь к целевому каталогу
+        target: Целевые расширения файлов
+        transformer: Наименование функции трансформации имени перемещаемого файла
+        """
+        self.__FILENAME_TRANSFORMER_TABLE = self.__build_filename_transformer()
+        self.__TRANSLITERATE_TABLE = self.__gen_table()
 
-        if isinstance(self.transformer, str):
-            self.transformer = globals().get(self.transformer)
+        self.dest_path = Path(path).absolute()
+        self.transformer = self._get_filename_transformer(transformer)
+        self.target_extensions = target
+
+    def run(self, work_dir: Path) -> int:
+        """Применить функцию трансформации ко всем целевым файлам в заданном каталоге"""
+        moved_count = sum(map(self._move, _make_glob(work_dir, self.target_extensions)))
+        print(f"Moved: {self.dest_path.absolute().__str__() + ' ':.<100} {moved_count}")
+        return moved_count
 
     def _move(self, path: Path) -> bool:
-        if path.suffix not in self.target:
-            return False
+        dest = self.dest_path / (self.transformer(path) + path.suffix)
 
-        dest = self.path / (self.transformer(path.stem) + path.suffix)
-
-        if path == dest:
+        if path.parent == self.dest_path:
             return False
 
         if dest.exists():
@@ -100,34 +89,62 @@ class FileMover:
 
         return True
 
-    def run(self, work_dir: Path) -> int:
-        """Применить функцию трансформации ко всем целевым файлам в заданном каталоге"""
-        moved_count = sum(map(self._move, work_dir.rglob("*")))
-        print(f"Moved: {self.path.absolute().__str__() + ' ':.<100} {moved_count}")
-        return moved_count
+    @staticmethod
+    def __gen_table() -> Dict[str, str]:
+        table = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+                 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '`', 'ы': 'y', 'ь': '`', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+        table.update({cyrillic.upper(): latin.upper() for cyrillic, latin in table.items()})
+        return table
+
+    def _transliterate(self, s: str) -> str:
+        return "".join(self.__TRANSLITERATE_TABLE.get(char, char) for char in s)
+
+    @staticmethod
+    def _camel_case(s: str) -> str:
+        return '_'.join(map(lambda word: word.capitalize(), s.split()))
+
+    @staticmethod
+    def _add_parent_catalog_path(path: Path) -> str:
+        """Добавить к имени файла родительский каталог"""
+        return f"{path.parent.stem}.{path.stem}"
+
+    def __build_filename_transformer(self) -> Dict[Optional[str], FileNameTransformer]:
+        return {
+            None: lambda path: path.stem,
+            "translit": lambda path: self._transliterate(path.stem),
+            "camel_case": lambda path: self._camel_case(path.stem),
+            "add_parent_catalog": self._add_parent_catalog_path,
+            "3d_print_special": lambda path: self._camel_case(self._transliterate(self._add_parent_catalog_path(path)))
+        }
+
+    def _get_filename_transformer(self, transformer_name: Optional[str]) -> FileNameTransformer:
+        return self.__FILENAME_TRANSFORMER_TABLE[transformer_name]
 
 
 class FileMoverCollection:
     """Коллекция менеджеров настроек каталогов"""
-    _file_movers: Sequence[FileMover]
 
     def __init__(self, json_file: Path) -> None:
         self._size_begin = _get_workdir_size_bytes()
 
         with open(json_file, "r") as f:
-            self._file_movers = tuple(FileMover(path, **data) for path, data in json.load(f).items())
+            self._file_movers: Sequence[FileMover] = tuple(self._get_file_movers(f))
 
-    def getMoversCount(self) -> int:
+    def get_movers_count(self) -> int:
         """Получить количество менеджеров"""
         return self._file_movers.__len__()
 
-    def getMemoryFreeBytes(self) -> int:
+    def get_memory_free_bytes(self) -> int:
         """Получить объем изменения папки проекта"""
         return _get_workdir_size_bytes() - self._size_begin
 
     def run(self) -> int:
         """Применить всех менеджеров в папке с проектом"""
         return sum(map(lambda m: m.run(_WORK_DIR), self._file_movers))
+
+    @staticmethod
+    def _get_file_movers(json_file: TextIO) -> Iterable[FileMover]:
+        return (FileMover(path, **data) for path, data in json.load(json_file).items())
 
 
 def main():
@@ -136,13 +153,13 @@ def main():
 
     file_mover_collection = FileMoverCollection(Path("config.json"))
 
-    print(f"Movers: {file_mover_collection.getMoversCount()}")
+    print(f"Movers: {file_mover_collection.get_movers_count()}")
     print(f"MOVED TOTAL: {file_mover_collection.run()}")
-    print(f"FREE: {_get_size_str(file_mover_collection.getMemoryFreeBytes())}")
+    print(f"FREE: {_get_size_str(file_mover_collection.get_memory_free_bytes())}")
 
     _set_shell_color(0x07)
 
-    input("DONE:  -> Enter <-")
+    _shell_timeout(5)
     _set_shell_color(0x00)
 
 
