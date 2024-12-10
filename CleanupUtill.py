d4171755774afa6ee56688d143bf67cc4e265d42
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-12-10 22:42:32 +0300

    Добавлены .STP модели для печать Обновлена платформа MIDI-Q

--- CleanupUtill.py
+++ CleanupUtill.py
@@ -10,10 +10,10 @@ from __future__ import annotations
 import json
 import os
 import sys
+from dataclasses import dataclass
 from itertools import chain
 from pathlib import Path
 from typing import Callable
-from typing import Dict
 from typing import Iterable
 from typing import Optional
 from typing import Sequence
@@ -73,7 +73,51 @@ def _make_glob(path: Path, extensions: Sequence[str]):
     return chain(*(path.rglob(f"*.{extension}") for extension in extensions))
 
 
-FileNameTransformer = Callable[[Path], str]
+@dataclass
+class PathTransformer:
+    """Обертка над Path"""
+
+    parent: Path
+    path: Path
+
+    def transliterate(self) -> PathTransformer:
+        """Имя файла будет транслитерировано"""
+        self._change_filename(self._transliterate(self.path.stem))
+        return self
+
+    def camel_case(self) -> PathTransformer:
+        """Имя файла будет форматировано в Camel_Case"""
+        self._change_filename(self._camel_case(self.path.stem))
+        return self
+
+    def add_parent(self) -> PathTransformer:
+        """Добавить к имени файла родительский каталог"""
+        self._change_filename(self._add_parent_catalog_path(self.path))
+        return self
+
+    __TRANSLITERATE_TABLE = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z',
+        'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
+        'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh',
+        'щ': 'sch', 'ъ': '`', 'ы': 'y', 'ь': '`', 'э': 'e', 'ю': 'yu', 'я': 'ya'
+    }
+
+    def _change_filename(self, new_name: str) -> None:
+        self.path = self.parent / (new_name + self.path.suffix)
+
+    def _transliterate(self, s: str) -> str:
+        return "".join(self.__TRANSLITERATE_TABLE.get(char, char) for char in s.lower())
+
+    @staticmethod
+    def _camel_case(s: str) -> str:
+        return '_'.join(map(lambda word: word.capitalize(), s.split()))
+
+    @staticmethod
+    def _add_parent_catalog_path(path: Path) -> str:
+        return f"{path.parent.stem}__{path.stem}"
+
+
+FileNameTransformer = Callable[[PathTransformer], PathTransformer]
 
 
 class FileMover:
@@ -86,8 +130,6 @@ class FileMover:
         target: Целевые расширения файлов
         transformer: Наименование функции трансформации имени перемещаемого файла
         """
-        self.__FILENAME_TRANSFORMER_TABLE = self.__build_filename_transformer()
-        self.__TRANSLITERATE_TABLE = self.__gen_table()
 
         self.dest_path = Path(path).absolute()
         self.transformer = self._get_filename_transformer(transformer)
@@ -104,10 +146,10 @@ class FileMover:
         return moved_count
 
     def _move(self, path: Path) -> bool:
-        dest = self.dest_path / (self.transformer(path) + path.suffix)
-
         if path.parent == self.dest_path:
             return False
+
+        dest = self.transformer(PathTransformer(self.dest_path, path)).path
 
         if dest.exists():
             os.remove(dest)
@@ -117,36 +159,16 @@ class FileMover:
 
         return True
 
-    @staticmethod
-    def __gen_table() -> Dict[str, str]:
-        table = {'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r',
-                 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '`', 'ы': 'y', 'ь': '`', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
-        table.update({cyrillic.upper(): latin.upper() for cyrillic, latin in table.items()})
-        return table
-
-    def _transliterate(self, s: str) -> str:
-        return "".join(self.__TRANSLITERATE_TABLE.get(char, char) for char in s)
-
-    @staticmethod
-    def _camel_case(s: str) -> str:
-        return '_'.join(map(lambda word: word.capitalize(), s.split()))
-
-    @staticmethod
-    def _add_parent_catalog_path(path: Path) -> str:
-        """Добавить к имени файла родительский каталог"""
-        return f"{path.parent.stem}.{path.stem}"
-
-    def __build_filename_transformer(self) -> Dict[Optional[str], FileNameTransformer]:
-        return {
-            None: lambda path: path.stem,
-            "translit": lambda path: self._transliterate(path.stem),
-            "camel_case": lambda path: self._camel_case(path.stem),
-            "add_parent_catalog": self._add_parent_catalog_path,
-            "3d_print_special": lambda path: self._camel_case(self._transliterate(self._add_parent_catalog_path(path)))
-        }
-
     def _get_filename_transformer(self, transformer_name: Optional[str]) -> FileNameTransformer:
         return self.__FILENAME_TRANSFORMER_TABLE[transformer_name]
+
+    __FILENAME_TRANSFORMER_TABLE = {
+        None: lambda path: path,
+        "translit": lambda path: path.transliterate(),
+        "camel_case": lambda path: path.camel_case(),
+        "add_parent_catalog": lambda path: path.add_parent(),
+        "3d_print_special": lambda path: path.add_parent().camel_case().transliterate()
+    }
 
 
 class FileMoverCollection:
