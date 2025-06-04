51c95b20c870b2cf9ff1d2ef100bf591e8579877
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-05 00:51:20 +0300

    section load

--- Meta/src/projectmaster/entities/impl/loader/section.py
+++ Meta/src/projectmaster/entities/impl/loader/section.py
@@ -13,18 +13,12 @@ class SectionLoader(EntityLoader[Section]):
     """Загрузчик разделов"""
 
     def load(self) -> Section:
-        t = list()
-
-        for module_type in iterDirs(self.folder()):
-            for module_class in iterDirs(module_type):
-                for module_version in iterDirs(module_class):
-                    t.append(UnitLoader(module_version).load())
-
-        pass
-
         return Section(
             units=tuple(
-                t
+                UnitLoader(unit_version).load()
+                for unit_type in iterDirs(self.folder())
+                for unit_kind in iterDirs(unit_type)
+                for unit_version in iterDirs(unit_kind)
             )
         )
 
