cd3d8c3a4d3f3edc9839c8f2ba30b7500cce78ae
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-06-05 23:08:44 +0300

    da

--- Meta/src/projectmaster/entities/impl/visitor/render/text.py
+++ Meta/src/projectmaster/entities/impl/visitor/render/text.py
@@ -3,6 +3,7 @@
 """
 
 from dataclasses import dataclass
+from dataclasses import field
 from typing import ClassVar
 from typing import TextIO
 
@@ -14,67 +15,102 @@ from projectmaster.entities.core.section import SectionEntity
 from projectmaster.entities.core.unit import UnitEntity
 
 
+@dataclass
+class IntendTextIOAdapter:
+    """Адаптер над TextIO для реализации записи с отступами"""
+
+    _intend: ClassVar = 4
+    _intend_string: ClassVar = ' ' * _intend
+
+    _source: TextIO
+    _intend_level: int = field(init=False, default=0)
+
+    def push(self) -> None:
+        """Внести отступ"""
+        self._intend_level += 1
+
+    def pop(self) -> None:
+        """Вынести отступ"""
+        self._intend_level -= 1
+        assert self._intend_level >= 0
+
+    def _write(self, s: str) -> None:
+        self._source.write(s)
+
+    def write(self, s: str = None) -> None:
+        """Записать"""
+
+        if s is not None:
+            self._write(self._intend_string * self._intend_level)
+            self._write(s)
+
+        self._source.write('\n')
+
+
 @dataclass(frozen=True)
 class TextRenderEntityVisitor(EntityVisitor):
-    metadata_name_width: ClassVar = 24
-    section_name_width: ClassVar = 32
+    metadata_name_width: ClassVar = 32
 
-    output: TextIO
-
-    def writeLine(self) -> None:
-        """Перенос строки"""
-        self.output.write('\n')
-
-    def writeTab(self) -> None:
-        """Отступ"""
-        self.output.write('\t')
+    out: IntendTextIOAdapter
 
     def visitMetadataEntity(self, metadata: MetadataEntity) -> None:
-        self.output.write(f"{metadata.getEntityName():{self.metadata_name_width}}")
+        s = f"{metadata.getEntityName():{self.metadata_name_width}}"
 
         if metadata.images:
-            self.output.write(f": images({len(metadata.images)})")
+            s += f": images({len(metadata.images)})"
+
+        self.out.write(s)
 
     def visitPartEntity(self, part: PartEntity) -> None:
         self.visitMetadataEntity(part.metadata)
 
+        self.out.push()
+
+        s = str()
+
         if part.prusa_project is not None:
-            self.writeTab()
-            self.output.write("prusa")
+            s += "prusa"
+
+        if s:
+            s += '\t'
 
         if part.transitions:
-            self.writeTab()
-            self.output.write('(' + ','.join(p.suffix for p in part.transitions) + ')')
+            s += tuple(p.suffix for p in part.transitions).__str__()
+
+        if s:
+            self.out.write(s)
+
+        self.out.pop()
 
     def visitUnitEntity(self, unit: UnitEntity) -> None:
         self.visitMetadataEntity(unit.metadata)
-        self.writeLine()
 
         for part in unit.parts:
-            self.writeTab()
+            self.out.push()
             self.visitPartEntity(part)
-            self.writeLine()
+            self.out.pop()
 
     def visitSectionEntity(self, section: SectionEntity) -> None:
-        self.output.write(f"{section.attributes.name:.<{self.section_name_width}} ({len(section.units)})")
-        self.writeLine()
+        self.out.write(f"{section.attributes.name} ({len(section.units)})")
 
-        self.writeTab()
-        self.output.write(f"{section.attributes.desc}")
-        self.writeLine()
+        self.out.push()
+        self.out.write(f"{section.attributes.desc}")
+        self.out.pop()
 
-        self.writeLine()
+        self.out.write()
 
         for unit in section.units:
+            self.out.push()
+
             self.visitUnitEntity(unit)
-            self.writeLine()
+            self.out.write()
+
+            self.out.pop()
 
     def visitProjectEntity(self, project: ProjectEntity) -> None:
-        self.output.write(f"РАЗДЕЛЫ: ({len(project.sections)})")
-        self.writeLine()
-
-        self.writeLine()
+        self.out.write(f"РАЗДЕЛЫ: ({len(project.sections)})")
+        self.out.write()
 
         for section in project.sections:
             self.visitSectionEntity(section)
-            self.writeLine()
+            self.out.write()
--- Meta/test/test_text_render_entity_visitor.py
+++ Meta/test/test_text_render_entity_visitor.py
@@ -2,11 +2,13 @@ import sys
 from pathlib import Path
 
 from projectmaster.entities.impl.loader.project import ProjectEntityLoader
+from projectmaster.entities.impl.visitor.render.text import IntendTextIOAdapter
 from projectmaster.entities.impl.visitor.render.text import TextRenderEntityVisitor
 
 path = Path(r"A:\Projects\OmniCore-Robotics\Модели")
 
 p = ProjectEntityLoader(path).load()
 
-v = TextRenderEntityVisitor(sys.stdout)
+v = TextRenderEntityVisitor(IntendTextIOAdapter(sys.stdout))
+
 v.visitProjectEntity(p)
