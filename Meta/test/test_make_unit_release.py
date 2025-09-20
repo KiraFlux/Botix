d1fa934e05ad0f34ac40216e0f0d22a07670c70a
JamahaW <153020210+JamahaW@users.noreply.github.com>
2025-09-20 12:19:32 +0300

    Автоматическое создание выпусков для производства

--- Meta/test/test_make_unit_release.py
+++ Meta/test/test_make_unit_release.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import Callable
 from typing import Optional
+from typing import Sequence
 
 from botix.core.entities import PartEntity
 from botix.core.entities import UnitEntity
@@ -13,52 +14,187 @@ from botix.core.registries import UnitEntityRegistry
 from botix.impl.loaders import ProjectEntityLoader
 
 
+def _link(s: str, path: str) -> str:
+    return f"[`{s}`]({path})"
+
+
+def _img(src: str, height: int) -> str:
+    return f'<img src="{src}" height="{height}">'
+
+
 def _date() -> str:
     return datetime.now().strftime("%Y.%m.%d")
+
+
+def _makeImagesTable(images: Sequence[Path], main_height: int) -> str:
+    images_total = len(images)
+
+    if images_total == 0:
+        return ""
+
+    main_image, *other_images = (i.name for i in images)
+
+    main_img_tag = _img(main_image, main_height)
+
+    if len(other_images) == 0:
+        return main_img_tag
+
+    other_image_height = main_height // (images_total - 1)
+
+    other_images_tags_string = '\n'.join((
+        _img(i, other_image_height) + "<br>"
+        for i in other_images
+    ))
+
+    return f"""
+<table>
+<tr valign="top">
+<td>
+{main_img_tag}
+</td>
+<td>
+{other_images_tags_string}
+</td>
+</tr>
+</table>
+    """
+
+
+def _makeHeader(unit: UnitEntity, current_date: str) -> str:
+    transitions = [
+        ('zip', f'./../{unit.metadata.getEntityName()}--{current_date}.zip'),
+    ]
+
+    if unit.transition_assembly:
+        transitions.append(('step', unit.transition_assembly.name))
+
+    transitions_string = ' '.join(
+        _link(name, path)
+        for name, path in transitions
+    )
+
+    return f"""
+# [{unit.metadata.getDisplayName()} {unit.metadata.getDisplayVersion()}](.)
+
+[Выпуск от {current_date}](.) {transitions_string}
+
+{_makeImagesTable(unit.metadata.images, 360)}
+
+# Детали
+
+"""
+
+
+def _makePartBlock(part: PartEntity, count: int, name_transformer: Callable[[str], str]) -> str:
+    images_html = '\n'.join(
+        f'<td>{_img(name_transformer(i.name), 180)}</td>'
+        for i in part.metadata.images
+    )
+
+    transitions = list(
+        (t.suffix, name_transformer(t.name))
+        for t in part.transitions
+    )
+
+    def _addProject(path: Optional[Path], slicer: str) -> None:
+        if path:
+            transitions.append((f"{path.suffix} ({slicer})", name_transformer(path.name)))
+
+    _addProject(part.prusa_project, "PrusaSlicer")
+    _addProject(part.orca_project, "OrcaSlicer")
+
+    transitions_string = ' '.join(
+        _link(name, path)
+        for name, path in transitions
+    )
+
+    return f"""
+<blockquote>
+
+## {part.metadata.getDisplayName()} - {count} шт.
+
+<table>
+<tr valign="top">
+{images_html}
+</tr>
+</table>
+
+{transitions_string}
+
+</blockquote>
+"""
+
+
+def _makeFooter() -> str:
+    return """
+---
+
+<sub>
+Файл сгенерирован инструментами проекта.
+
+[LICENCE](https://github.com/KiraFlux/Botix/blob/main/LICENSE)
+
+</sub>
+"""
 
 
 def _makeUnitAssemblyFiles(output_folder: Path, unit: UnitEntity, parts_registry: PartsSectionRegistry) -> None:
     assert unit.attributes is not None, f"Attributes (.unit) must exists"
 
-    part_registry = PartEntityRegistry(unit, parts_registry)
-
     unit_release_folder = output_folder / unit.metadata.getEntityName()
 
-    def _move(p: Optional[Path], name_transformer: Callable[[str], str] = str) -> None:
-        if p:
-            shutil.copyfile(p, unit_release_folder / name_transformer(p.name))
-
-    archive_path = output_folder / f"{unit.metadata.getEntityName()}--{_date()}"
+    #
 
     if unit_release_folder.exists():
         shutil.rmtree(unit_release_folder)
 
     unit_release_folder.mkdir(parents=True)
 
-    _move(unit.transition_assembly)
+    #
 
-    for image in unit.metadata.images:
-        _move(image)
+    def _move(p: Optional[Path], name_transformer: Callable[[str], str] = str) -> None:
+        if p:
+            shutil.copyfile(p, unit_release_folder / name_transformer(p.name))
 
-    for part_key, count in unit.attributes.part_count_map.items():
-        part: Optional[PartEntity] = part_registry.get(part_key)
+    current_date = _date()
 
-        if part is None:
-            print(f"cannot find: {part_key}")
+    with open(unit_release_folder / "README.md", "wt") as out:
 
-        else:
-            def _nameTransformer(s: str) -> str:
-                return f"{count}x--{s}"
+        out.write(_makeHeader(unit, current_date))
 
-            for transition in part.transitions:
-                _move(transition, _nameTransformer)
+        _move(unit.transition_assembly)
 
-            for image in part.metadata.images:
-                _move(image, _nameTransformer)
+        for image in unit.metadata.images:
+            _move(image)
 
-            if part.prusa_project:
-                _move(part.prusa_project, _nameTransformer)
+        part_registry = PartEntityRegistry(unit, parts_registry)
 
+        for part_key, count in unit.attributes.part_count_map.items():
+            part: Optional[PartEntity] = part_registry.get(part_key)
+
+            if part is None:
+                print(f"cannot find: {part_key}")
+
+            else:
+                def _nameTransformer(s: str) -> str:
+                    return f"{count}x--{s}"
+
+                out.write(_makePartBlock(part, count, _nameTransformer))
+
+                for transition in part.transitions:
+                    _move(transition, _nameTransformer)
+
+                for image in part.metadata.images:
+                    _move(image, _nameTransformer)
+
+                if part.prusa_project:
+                    _move(part.prusa_project, _nameTransformer)
+
+        out.write(_makeFooter())
+
+    #
+
+    archive_path = output_folder / f"{unit.metadata.getEntityName()}--{current_date}"
     shutil.make_archive(
         base_name=str(archive_path),
         format="zip",
@@ -70,15 +206,18 @@ def _makeUnitAssemblyFiles(output_folder: Path, unit: UnitEntity, parts_registry
 
 
 def _main() -> None:
-    root = Path("A:/Projects/Botix/Meta/test/Mock/Модели")
+    root = Path("A:/Projects/Botix")
+    output_folder = root / "Производство"
 
-    project = ProjectEntityLoader(root).load()
+    project = ProjectEntityLoader(root / "Модели").load()
     units = UnitEntityRegistry(project)
+    print('\n'.join(map(str, units.getAll().keys())))
     parts = PartsSectionRegistry(project)
+    print('\n'.join(map(str, units.getAll().keys())))
 
-    unit = units.get(UnitKey("Шасси/Тест-Вариант-v2"))
+    unit = units.get(UnitKey("Шасси/MidiQ-Всош-v2"))
 
-    _makeUnitAssemblyFiles(root / "Производство", unit, parts)
+    _makeUnitAssemblyFiles(output_folder, unit, parts)
 
     return
 
--- Производство/MidiQ-Всош-v2/README.md
+++ Производство/MidiQ-Всош-v2/README.md
@@ -1,24 +1,105 @@
-<div align="center">
 
-# MidiQ Р’СЃРѕС€ v2
+# [MidiQ Всош v2](.)
 
-`Р’С‹РїСѓСЃРє 2025.09.10`
+[Выпуск от 2025.09.20](.) [`zip`](./../MidiQ-Всош-v2--2025.09.20.zip) [`step`](MidiQ-Всош-v2.stp)
 
-</div>
+
+<table>
+<tr valign="top">
+<td>
+<img src="MidiQ-Всош-v2-0.jpg" height="360">
+</td>
+<td>
+<img src="MidiQ-Всош-v2-1.jpg" height="180"><br>
+<img src="MidiQ-Всош-v2-2.jpg" height="180"><br>
+</td>
+</tr>
+</table>
+    
+
+# Детали
+
+
+<blockquote>
+
+## Платформа - 1 шт.
+
+<table>
+<tr valign="top">
+<td><img src="1x--Платформа-v1-0.png" height="180"></td>
+<td><img src="1x--Платформа-v1-1.png" height="180"></td>
+</tr>
+</table>
+
+[`.stp`](1x--Платформа-v1.stp) [`.dxf`](1x--Платформа-v1.dxf) [`.3mf (PrusaSlicer)`](1x--Платформа-v1.prusa.3mf)
+
+</blockquote>
+
+<blockquote>
+
+## Стенка Боковая - 2 шт.
+
+<table>
+<tr valign="top">
+<td><img src="2x--Стенка-Боковая-v1-0.png" height="180"></td>
+<td><img src="2x--Стенка-Боковая-v1-1.png" height="180"></td>
+</tr>
+</table>
+
+[`.stp`](2x--Стенка-Боковая-v1.stp) [`.dxf`](2x--Стенка-Боковая-v1.dxf)
+
+</blockquote>
+
+<blockquote>
+
+## Стенка Верхняя - 1 шт.
+
+<table>
+<tr valign="top">
+<td><img src="1x--Стенка-Верхняя-v1-0.png" height="180"></td>
+<td><img src="1x--Стенка-Верхняя-v1-1.png" height="180"></td>
+</tr>
+</table>
+
+[`.stp`](1x--Стенка-Верхняя-v1.stp) [`.dxf`](1x--Стенка-Верхняя-v1.dxf)
+
+</blockquote>
+
+<blockquote>
+
+## Стенка Задняя - 1 шт.
+
+<table>
+<tr valign="top">
+<td><img src="1x--Стенка-Задняя-v2-0.png" height="180"></td>
+<td><img src="1x--Стенка-Задняя-v2-1.png" height="180"></td>
+</tr>
+</table>
+
+[`.stp`](1x--Стенка-Задняя-v2.stp) [`.dxf`](1x--Стенка-Задняя-v2.dxf) [`.3mf (PrusaSlicer)`](1x--Стенка-Задняя-v2.prusa.3mf)
+
+</blockquote>
+
+<blockquote>
+
+## Рамка - 2 шт.
+
+<table>
+<tr valign="top">
+<td><img src="2x--Рамка-v4-0.png" height="180"></td>
+<td><img src="2x--Рамка-v4-1.png" height="180"></td>
+</tr>
+</table>
+
+[`.stp`](2x--Рамка-v4.stp) [`.dxf`](2x--Рамка-v4.dxf) [`.3mf (PrusaSlicer)`](2x--Рамка-v4.prusa.3mf)
+
+</blockquote>
 
 ---
 
-## Р¤РѕСЂРјР°С‚С‹ С„Р°Р№Р»РѕРІ
+<sub>
+Файл сгенерирован инструментами проекта.
 
-РљРѕР»РёС‡РµСЃС‚РІРѕ РєРѕРїРёР№ РґРµС‚Р°Р»РµР№ СѓРєР°Р·Р°РЅРѕ С‡РµСЂРµР· РїСЂРµС„РёРєСЃ `nx` (`2x--Р”РµС‚Р°Р»СЊ` - 2 С€С‚.)
+[LICENCE](https://github.com/KiraFlux/Botix/blob/main/LICENSE)
 
-- 3D-РџРµС‡Р°С‚СЊ `.stp`
-- Р›Р°Р·РµСЂРЅР°СЏ СЂРµР·РєР° `.dxf`
-
-## РћСЃРЅРѕРІРЅРѕР№ СЂРµРїРѕР·РёС‚РѕСЂРёР№
-
-[https://github.com/KiraFlux/Botix](https://github.com/KiraFlux/Botix)
-
-## Р›РёС†РµРЅР·РёСЏ
-
-**GNU GPL v3.0** [LICENSE](https://github.com/KiraFlux/Botix/blob/main/LICENSE)
+</sub>
